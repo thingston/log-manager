ed2f2024e2e4b5e334712dbee8cc844ba0a9946c
Pedro Ferreira <pedro@thingston.com>
2022-07-15 15:06:18 +0100

    Add error log support

--- src/LogSettings.php
+++ src/LogSettings.php
@@ -7,6 +7,7 @@ namespace Thingston\Log;
 use Monolog\Handler\NullHandler;
 use Monolog\Handler\RotatingFileHandler;
 use Monolog\Handler\StreamHandler;
+use Monolog\Handler\ErrorLogHandler;
 use Psr\Log\LogLevel;
 use Thingston\Settings\AbstractSettings;
 
@@ -20,6 +21,7 @@ final class LogSettings extends AbstractSettings
     public const LOG_STREAM = 'stream';
     public const LOG_NULL = 'null';
     public const LOG_ROTATING = 'rotating';
+    public const LOG_ERROR = 'error';
 
     /**
      * @param array<string, array<mixed>|scalar|\Thingston\Settings\SettingsInterface> $settings
@@ -27,33 +29,34 @@ final class LogSettings extends AbstractSettings
     public function __construct(array $settings = [])
     {
         parent::__construct(array_merge([
-            self::DEFAULT => 'stream',
-            self::LOG_STACK => [self::LOG_STREAM],
+            self::DEFAULT => self::LOG_STACK,
+            self::LOG_STACK => [self::LOG_STREAM, self::LOG_ERROR],
             self::LOG_NULL => [
-                [
-                    self::HANDLER => NullHandler::class,
-                    self::ARGUMENTS => [
-                        'level' => LogLevel::DEBUG,
-                    ],
+                self::HANDLER => NullHandler::class,
+                self::ARGUMENTS => [
+                    'level' => LogLevel::DEBUG,
                 ],
             ],
             self::LOG_STREAM => [
-                [
-                    self::HANDLER => StreamHandler::class,
-                    self::ARGUMENTS => [
-                        'stream' => sys_get_temp_dir() . '/thingston.log',
-                        'level' => LogLevel::DEBUG,
-                    ],
+                self::HANDLER => StreamHandler::class,
+                self::ARGUMENTS => [
+                    'stream' => sys_get_temp_dir() . '/thingston.log',
+                    'level' => LogLevel::DEBUG,
                 ],
             ],
             self::LOG_ROTATING => [
-                [
-                    self::HANDLER => RotatingFileHandler::class,
-                    self::ARGUMENTS => [
-                        'filename' => sys_get_temp_dir() . '/thingston.log',
-                        'maxFiles' => 7,
-                        'level' => LogLevel::DEBUG,
-                    ],
+                self::HANDLER => RotatingFileHandler::class,
+                self::ARGUMENTS => [
+                    'filename' => sys_get_temp_dir() . '/thingston.log',
+                    'maxFiles' => 7,
+                    'level' => LogLevel::DEBUG,
+                ],
+            ],
+            self::LOG_ERROR => [
+                self::HANDLER => ErrorLogHandler::class,
+                self::ARGUMENTS => [
+                    'messageType' => 0,
+                    'level' => LogLevel::ERROR,
                 ],
             ],
         ], $settings));
